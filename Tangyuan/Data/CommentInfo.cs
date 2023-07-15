f9fde9dd11145cdc05bea5b93e866ad7db79f078
线粒体XianlitiCN <xianliticn@outlook.com>
2023-07-15 11:30:21 +0800

    评论数据类型完成

---
+++ Tangyuan/Data/CommentInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangyuan.Data
+{
+    internal class CommentInfo
+    {
+        internal uint CommentID { get; private set; }
+        internal uint UserID { get; private set; }
+        internal uint PostID { get; private set; }
+        internal DateTime Date { get; private set; }
+        internal uint Likes { get; private set; }
+        internal bool IsReply { get; private set; }
+        internal uint ReplyID { get; private set; }
+        internal string Content { get; private set; }
+
+        internal CommentInfo(uint commentID, uint userID, uint postID, DateTime date, uint likes, string content, bool isReply = false, uint replyID = 0)
+        {
+            CommentID = commentID;
+            UserID = userID;
+            PostID = postID;
+            Date = date;
+            Likes = likes;
+            Content = content;
+            IsReply = isReply;
+            ReplyID = replyID;
+        }
+
+    }
+}
