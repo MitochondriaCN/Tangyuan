a7f49a50eff32b54175739ec6a869d0c66d98dcf
线粒体XianlitiCN <xianliticn@outlook.com>
2023-07-16 11:35:35 +0800

    本地数据库初步，字体修改

---
+++ Tangyuan/Data/LocalSQLDataHelper.cs
@@ -0,0 +1,38 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangyuan.Data
+{
+    internal static class LocalSQLDataHelper
+    {
+        private const string databaseName = "local_tangyuan_db.db3";
+        private const SQLiteOpenFlags flags =
+            SQLiteOpenFlags.ReadWrite |
+            SQLiteOpenFlags.Create |
+            SQLiteOpenFlags.SharedCache;
+        private static string databasePath = Path.Combine(FileSystem.AppDataDirectory, databaseName);
+
+        private static SQLiteAsyncConnection conn;
+
+        internal class LocalLoginInfo
+        {
+            internal uint LoginUserID { get; private set; }
+            internal string LoginUserPassword { get; private set; }
+        }
+
+        static LocalSQLDataHelper()
+        {
+            conn = new SQLiteAsyncConnection(databasePath, flags);
+            conn.CreateTableAsync(typeof(LocalLoginInfo));
+        }
+
+        internal static async Task<LocalLoginInfo> GetLoginInfoAsync()
+        {
+            return await conn.Table<LocalLoginInfo>().FirstAsync();
+        }
+    }
+}
--- Tangyuan/MauiProgram.cs
+++ Tangyuan/MauiProgram.cs
@@ -9,7 +9,7 @@ public static class MauiProgram
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
 			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("NotoSansSC-Regular,otf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
 
