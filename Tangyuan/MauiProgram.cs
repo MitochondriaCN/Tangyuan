d8681494e984a26fe03682f138475ac8474bc93f
线粒体XianlitiCN <xianliticn@outlook.com>
2023-07-27 20:44:09 +0800

    加入华康俪宋W5

--- Tangyuan/MauiProgram.cs
+++ Tangyuan/MauiProgram.cs
@@ -15,6 +15,7 @@ public static class MauiProgram
 				fonts.AddFont("HiraginoSansGB-W3.ttf", "OpenSansRegular");
 				fonts.AddFont("HiraginoSansGB-W6.ttf", "HiraginoW6");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("LiSong-W5.ttf", "LisongW5");
 			});
 
 		return builder.Build();
